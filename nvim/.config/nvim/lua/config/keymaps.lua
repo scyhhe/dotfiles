315d921dfec8e2e3ac3a0abda21d65619486a805
scyhhe <dimitrov.toporkov@gmail.com>
2026-04-20 15:53:18 +0200

    cleanup useless nvim dotfiles

--- nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<leader>rr", function()
-  return ":IncRename " .. vim.fn.expand("<cword>")
-end, { expr = true, desc = "Rename All Occurences" })
